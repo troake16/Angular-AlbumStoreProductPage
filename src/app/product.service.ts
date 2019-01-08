69e4b0f39c0a96e2db8233329d540a9ff580f963
Tim Roake <troake16@gmail.com>
2019-01-08 11:07:17 -0500

    Fixing variable casing

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,11 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _albumURL = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumURL).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
