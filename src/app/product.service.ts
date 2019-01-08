7b2c2b0de4f21ea692f4f0fd57e95f8015710183
Tim Roake <troake16@gmail.com>
2019-01-08 10:58:20 -0500

    Create a Service to Get Album Data Module

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumURL = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumURL).map((response) => response.json());
+  }
+}
